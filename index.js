f526541faf0be48f0f93ea1a8b3ff82c14b57ee9
Omar Tejada <omar14tejada@gmail.com>
2024-10-16 16:00:52 -0400

    configuring logging for methods.

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
 app.use(cors())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status - :response-time ms :body'))
 
 const generateID = () => {
     const maxID = persons.length > 0
